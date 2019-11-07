62e206d593aeaa2a8acd1ce70946746ff42f2aa8
jakep5 <jakepagel1@yahoo.com>
2019-11-07 11:15:09 -0600

    add delete list functionality

--- src/app.js
+++ src/app.js
@@ -132,7 +132,53 @@ app.post('/card', (req, res) => {
         .status(201)
         .location(`http://localhost:8000/card/${id}`)
         .json(card);
-        
+
+})
+
+app.post('/list', (req, res) => {
+    const {header, cardIds = []} = req.body;
+
+    if(!header) {
+        logger.error(`Header is required`);
+        return res
+            .status(400)
+            .send('Invalid data');
+    }
+
+    //checks card IDs
+    if (cardIds.length > 0) {
+        let valid = true;
+        cardIds.forEach(cid => {
+            const card = cards.find(c => c.id == cid);
+            if(!card) {
+                logger.error(`Card with id ${cid} not found in cards array`);
+                valid = false;
+            }
+        })
+
+        if(!valid) {
+            return res
+                .status(400)
+                .send('Invalid data');
+        }
+    }
+
+    const id = uuid();
+
+    const list = {
+        id,
+        header,
+        cardIds
+    };
+
+    lists.push(list);
+
+    logger.info(`List with id ${id} created`);
+
+    res
+        .status(201)
+        .location(`http://localhost:8000/list/${id}`)
+        .json({id});
 })
 
 app.use(function errorHandler(error, req, res, next) {
