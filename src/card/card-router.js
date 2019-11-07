91ac866b245b256c6fddfba60a53cd0bd1501869
jakep5 <jakepagel1@yahoo.com>
2019-11-07 11:38:09 -0600

    migrate card/:id endpoint GET and DELETE to card-router.js

--- src/card/card-router.js
+++ src/card/card-router.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const uuid = require('uuid/v4')
+const logger = require('../logger')
+
 
 const cardRouter = express.Router()
 const bodyParser = express.json()
@@ -56,10 +59,42 @@ cardRouter
 cardRouter
     .route('/card/:id')
     .get((req, res) => {
+        const { id } = req.params;
+        const card = cards.find(c => c.id == id);
 
+        if (!card) {
+            logger.error(`Card with id ${id} not found`);
+            return res
+                .status(404)
+                .send('Card not found');
+        }
+
+        res.json(card)
     })
     .delete((req, res) => {
+        const {id} = req.params;
 
+        const cardIndex = cards.findIndex(c => c.id == id);
+
+        if(cardIndex === -1) {
+            logger.error(`Card with id ${id} not found.`);
+            return res
+                .status(404)
+                .send('Not found');
+        }
+
+        lists.forEach(list => {
+            const cardIds = list.cardIds.filter(cid => cid !== id)
+            list.cardIds =  cardIds;
+        });
+
+        cards.splice(cardIndex, 1);
+
+        logger.info(`Card with id ${id} deleted`);
+
+        res
+            .status(204)
+            .end();
     })
 
 module.exports = cardRouter
