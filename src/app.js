6a8e0e5e2700dbf8420356541149f558ebdeeb3b
jakep5 <jakepagel1@yahoo.com>
2019-11-07 10:46:35 -0600

    add get endpoints for card/lists

--- src/app.js
+++ src/app.js
@@ -26,12 +26,75 @@ const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
     : 'common';
 
+const cards = [{
+    id: 1,
+    title: 'Task One',
+    content: 'This is card one'
+}];
+
+const lists = [{
+    id: 1,
+    header: 'List One',
+    cardIds: [1]
+}]
+
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(express.json());
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization') //gets the value put into the authorization header
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`) //logs the failed request to the console using Winston logger
+        return res.status(401).json({error: 'Unauthorized request' })
+    }
+
+    next();
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, boilerplate!')
+})
+
+app.get('/card', (req, res) => {
+    res
+        .json(cards);
+})
+
+app.get('/card/:id', (req, res) => {
+    const { id } = req.params;
+    const card = cards.find(c => c.id == id);
+
+    if (!card) {
+        logger.error(`Card with id ${id} not found`);
+        return res
+            .status(404)
+            .send('Card not found');
+    }
+
+    res.json(card)
+})
+
+app.get('/list', (req, res) => {
+     res
+        .json(lists);
+})
+
+app.get('/list/:id', (req, res) => {
+    const { id } = req.params;
+    const lsit = lists.find(li => li.id == id);
+
+    if (!list) {
+        logger.error(`List with id ${id} not found.`);
+        return res
+            .status(404)
+            .send('List not found');
+    }
+
+    res.json(list);
 })
 
 app.use(function errorHandler(error, req, res, next) {
